e8cc853b59ee3ccd5d445326e296a2f8a04569b0
Giraffeyy <127700862+Giraffeyy@users.noreply.github.com>
2024-05-29 20:50:36 -0500

    Delete createCanvas.js

--- createCanvas.js
+++
@@ -1,7 +0,0 @@
-// Example using the canvas library
-const { createCanvas } = require('canvas');
-const canvas = createCanvas(videoData.width, videoData.height);
-const context = canvas.getContext('2d');
-
-// Generate frames based on your video data
-// ...
